414459067d2cf496d090e9d5f49e18ebedbd1684
Stéphane Laurent <laurent_step@outlook.fr>
2020-09-29 08:20:03 +0000

    version 0.1.0

---
+++ README.md
@@ -0,0 +1,27 @@
+# **prettifyAddins**
+
+### RStudio addins to prettify JavaScript / JSX / HTML / CSS / SCSS / Markdown.
+
+**JavaScript example:**
+
+![](https://raw.githubusercontent.com/stla/prettifyAddins/master/inst/screenshots/prettifyAddins_js.gif)
+
+The **Markdown** prettifier also prettifies the code blocks:
+
+![](https://raw.githubusercontent.com/stla/prettifyAddins/master/inst/screenshots/prettifyAddins_md.gif)
+
+___
+
+# __Copies of license agreements__
+
+The 'prettifyAddins' package as a whole is distributed under GPL-3 (GNU
+GENERAL PUBLIC LICENSE version 3).
+
+It includes other open source software components:
+
+- **Prettier**, https://github.com/prettier/prettier
+- **indent.js**, https://github.com/zebzhao/indent.js
+- **CodeMirror**, https://github.com/codemirror/CodeMirror
+
+Full copies of the license agreements used by these components are included
+in the file [LICENSE.note](https://github.com/stla/prettifyAddins/blob/master/LICENSE.note).
---
+++ inst/shinyApp/server.R
@@ -0,0 +1,68 @@
+library(rstudioapi)
+
+context <- getOption("prettify.context")
+language <- getOption("prettify.language")
+code <- getOption("prettify.code")
+id <- context[["id"]]
+codemirror <- getOption("prettify.codemirror")
+
+action <- getOption("prettify.action")
+
+if(action == "prettify"){
+  parser <- switch(
+    language,
+    css = "css",
+    html = "html",
+    rhtml = "html",
+    javascript = "babel",
+    jsx = "babel",
+    markdown = "markdown",
+    scss = "css"
+  )
+}else{
+  if(codemirror){
+    parser <- language
+  }else{
+    parser <- switch(
+      language,
+      css = "css",
+      html = "html",
+      rhtml = "html",
+      javascript = "js",
+      jsx = "js",
+      scss = "css"
+    )
+  }
+}
+
+
+shinyServer(function(input, output, session){
+
+  observeEvent(input[["cancel"]], {
+    stopApp()
+  })
+
+  observeEvent(input[["done"]], {
+    setDocumentContents(text = input[["prettyCode"]], id = id)
+    stopApp()
+  })
+
+  session$sendCustomMessage(
+    action,
+    list(code = code, parser = parser)
+  )
+
+  observeEvent(input[["prettifyError"]], {
+    message("An error occured. This is possibly due to invalid code.")
+    stopApp()
+  })
+
+  observeEvent(input[["prettyCode"]], {
+    req(!codemirror)
+    updateAceEditor(
+      session, "editor",
+      mode = language, value = input[["prettyCode"]]
+    )
+  })
+
+})
